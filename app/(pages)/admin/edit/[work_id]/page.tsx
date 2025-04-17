f545452797159e815a538be36fec17f34f1dd6b5
Masaki Nagura <m.nagura12@gmail.com>
2025-04-17 13:46:49 +0900

    fix vercel compile error

--- app/(pages)/admin/edit/[work_id]/page.tsx
+++ app/(pages)/admin/edit/[work_id]/page.tsx
@@ -4,26 +4,24 @@ import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 import { Work } from "@/app/types/Work";
-
-interface AdminEditProps {
-  params: {
-    work_id: string;
-  };
-}
-
-export default function AdminEditWrapper({ params }: AdminEditProps) {
-  return <AdminEdit params={params} />;
-}
-
-async function AdminEdit({ params }: AdminEditProps) {
+export default async function AdminEdit({
+  params,
+}: {
+  params: { work_id: string };
+}) {
+  const { work_id } = await params;
   const session = await getServerSession(authOptions);
   if (!session) {
-    redirect("/admin?callbackUrl=/admin/edit/" + params.work_id);
+    redirect("/admin?callbackUrl=/admin/edit/" + work_id);
   }
   const works = await getWorkListUtil(session?.user.user_id);
-  const work = works.find((work: Work) => work.id === params.work_id);
+  const work = works.find((work: Work) => work.id === work_id);
   if (!work) {
-    return <div>Work not found</div>;
+    return (
+      <h1 className="text-5xl font-bold item-center text-center pt-20">
+        Work not found
+      </h1>
+    );
   }
   return <Form initialData={work} mode="update" />;
 }
