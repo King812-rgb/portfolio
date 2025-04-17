e544b930bdcf76d10272d0fdea170907f4f7dc29
Masaki Nagura <m.nagura12@gmail.com>
2025-04-17 13:07:24 +0900

    fix vercel compile error

--- app/(pages)/admin/edit/[work_id]/page.tsx
+++ app/(pages)/admin/edit/[work_id]/page.tsx
@@ -4,11 +4,14 @@ import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 import { Work } from "@/app/types/Work";
-export default async function AdminEdit({
-  params,
-}: {
-  params: { work_id: string };
-}) {
+
+interface PageProps {
+  params: {
+    work_id: string;
+  };
+}
+
+export default async function AdminEdit({ params }: PageProps) {
   const session = await getServerSession(authOptions);
   if (!session) {
     redirect("/admin?callbackUrl=/admin/edit/" + params.work_id);
